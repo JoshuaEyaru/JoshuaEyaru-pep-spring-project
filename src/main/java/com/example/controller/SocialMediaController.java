b7683e577e586e79212ec78017944a3442e05d92
Joshua Eyaru <emitu.joshua33@gmail.com>
2024-12-19 10:47:31 +0000

    remove unresolved imports

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.azul.crs.client.Response;
+
 import com.example.entity.Account;
 import com.example.entity.Message;
 import com.example.exception.ClientErrorException;
